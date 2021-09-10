bc95b0437f11e1112ca54842737eefcbefe945ec
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2021-09-10 15:26:53 -0300

    Update meat-api
    Finalizado o Curso da Udemy

--- meat-api/server/server.ts
+++ meat-api/server/server.ts
@@ -2,8 +2,10 @@ import * as fs from 'fs'
 
 import * as restify from 'restify'
 import * as mongoose from 'mongoose'
+import * as corsMiddleware from 'restify-cors-middleware'
 
 import {environment} from '../common/environment'
+import {logger} from '../common/logger'
 import {Router} from '../common/router'
 import {mergePatchBodyParser} from './merge-patch.parser'
 import {handleError} from './error.handler'
@@ -28,6 +30,7 @@ export class Server {
         const options: restify.ServerOptions = {
           name: 'meat-api',
           version: '1.0.0',
+          log: logger
         }
         if(environment.security.enableHTTPS){
           options.certificate = fs.readFileSync(environment.security.certificate),
@@ -36,6 +39,21 @@ export class Server {
 
         this.application = restify.createServer(options)
 
+        const corsOptions: corsMiddleware.Options = {
+          preflightMaxAge: 86400,
+          origins: ['*'],
+          allowHeaders: ['authorization'],
+          exposeHeaders: ['x-custom-header']
+        }
+        const cors: corsMiddleware.CorsMiddleware = corsMiddleware(corsOptions)
+
+        this.application.pre(cors.preflight)
+
+        this.application.pre(restify.plugins.requestLogger({
+          log: logger
+        }))
+
+        this.application.use(cors.actual)
         this.application.use(restify.plugins.queryParser())
         this.application.use(restify.plugins.bodyParser())
         this.application.use(mergePatchBodyParser)
@@ -51,6 +69,16 @@ export class Server {
         })
 
         this.application.on('restifyError', handleError)
+        //(req, resp, route, error)
+        /*this.application.on('after', restify.plugins.auditLogger({
+          log: logger,
+          event: 'after',
+          server: this.application
+        }))
+
+        this.application.on('audit', data=>{
+
+        })*/
 
       }catch(error){
         reject(error)
